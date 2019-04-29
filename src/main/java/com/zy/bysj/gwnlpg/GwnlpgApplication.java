5a7fb8a02ec007a13428ac28a6271630784fa098
zy <m18235421197@163.com>
2019-04-29 23:53:09 +0800

    commit init file

---
+++ src/main/java/com/zy/bysj/gwnlpg/GwnlpgApplication.java
@@ -0,0 +1,13 @@
+package com.zy.bysj.gwnlpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwnlpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwnlpgApplication.class, args);
+	}
+
+}
